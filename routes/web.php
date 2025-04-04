34fd8e7861794fa5c4d1f2c24263b4ade906a7eb
Michal Škoula <michal@skoula.com>
2025-04-04 20:31:45 +0200

    Project setup

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+// Dashboard
+// Data table
+// Card view of devices
+//		Single view of details
+// Recommendations panel
