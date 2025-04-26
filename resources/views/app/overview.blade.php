845e22d7aec5b62c037e443f34f567835bce3186
Michal Škoula <michal@skoula.com>
2025-04-27 00:44:21 +0200

    Update dashboard design

--- resources/views/app/overview.blade.php
+++ resources/views/app/overview.blade.php
@@ -1,4 +1,12 @@
 <x-layout.layout title="Hi mom">
+    <x-dock></x-dock>
+    <div class="w-full flex justify-between items-center mb-10">
+        <div>
+            <h1 class="text-white text-3xl font-medium mb-1">Dobré odpoledne</h1>
+            <p class="text-gray-body">Zde je váš přehled ovzduší</p>
+        </div>
+        <x-logo class="h-8"/>
+    </div>
     <div class="w-full grid grid-cols-5 gap-6 mb-8">
         <livewire:live-sensor-readout :data-type="\App\Enums\SensorDataType::TEMPERATURE"/>
         <livewire:live-sensor-readout :data-type="\App\Enums\SensorDataType::PRESSURE"/>
